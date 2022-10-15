59d133e72ab1fec9cea2284a354b784bc7311c42
Mr.Tea7171 <chaipanch98@gmail.com>
2022-10-15 20:14:32 +0530

    Deployment Changes

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,28 @@
 module.exports = {
- HOST : "localhost",
- USER : "root",
- PASSWORD : "",
- DB : "relevel",
- dialect : "mysql",
- pool :{
- max :5,
- min :0,
- acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
- idle :10000 // maximum time in ms that a connection can be idle before being released
- }
-}
+  development: {
+    HOST: 'localhost',
+    USER: 'root',
+    PASSWORD: '',
+    DB: 'relevel',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000 // maximum time in ms that a connection can be idle before being released
+    }
+  },
+  production: {
+    HOST: 'sql6.freemysqlhosting.net',
+    USER: 'sql6526755',
+    PASSWORD: 'l1Hk1UqfbI',
+    DB: 'sql6526755',
+    dialect: 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000, //max time in ms that a pool will try to get connection before throwing error
+      idle: 10000 // maximum time in ms that a connection can be idle before being released
+    }
+  }
+};
